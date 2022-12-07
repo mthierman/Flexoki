6f5b57797260ee7595743a7506d8c98a0d730623
mthierman <mthierman@gmail.com>
2022-12-07 13:51:25 -0700

    Build script update

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 $Repo = $PSScriptRoot
 $Build = Join-Path -Path $Repo -ChildPath "build"
-$Themes = Join-Path -Path $Repo -ChildPath "themes"
+$ThemesFrom = Join-Path -Path $Repo -ChildPath "themes"
 $ThemesTo = Join-Path -Path $Build -ChildPath "themes"
 $PackageFrom = Join-Path -Path $Repo -ChildPath "package.json"
 $PackageTo = Join-Path -Path $Build -ChildPath "package.json"
-$PackageNlsFrom = Join-Path -Path $Repo -ChildPath "package.nls.json"
+$PackageNlsFrom = Join-Path -Path $Repo -ChildPath "package.nls.build.json"
 $PackageNlsTo = Join-Path -Path $Build -ChildPath "package.nls.json"
 
 if (!(Test-Path -Path $Build)) { New-Item -ItemType Directory $Build }
@@ -12,6 +12,8 @@ if (!(Test-Path -Path $Build)) { New-Item -ItemType Directory $Build }
 if (Test-Path -Path $PackageTo) { Remove-Item -Path $PackageTo }
 Copy-Item -Path $PackageFrom -Destination $Build
 
-if (Test-Path -Path $ThemesTo) { Remove-Item -Path $ThemesTo }
-Copy-Item -Path $PackageNlsFrom -Destination $Build
+if (Test-Path -Path $PackageNlsTo) { Remove-Item -Path $PackageNlsTo }
+Copy-Item -Path $PackageNlsFrom -Destination $Build\package.nls.json
 
+if (Test-Path -Path $ThemesTo) { Remove-Item -Path $ThemesTo -Recurse }
+Copy-Item -Path $ThemesFrom -Destination $Build -Recurse
