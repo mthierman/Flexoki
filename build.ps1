58c5249aaffad4a13c037a3f468a6491659f22a2
mthierman <mthierman@gmail.com>
2023-10-24 22:30:46 -0600

    Flexoki dark initial colors

--- build.ps1
+++
@@ -1,9 +0,0 @@
-if (!(Test-Path -Path "build")) { New-Item -ItemType Directory "build" }
-
-Copy-Item -Path "package.json" -Destination "build/package.json" -Force
-Copy-Item -Path "package.nls.json" -Destination "build/package.nls.json" -Force
-
-Push-Location
-Set-Location build
-New-Item -ItemType SymbolicLink -Path "themes" -Target ".\..\themes" -Force
-Pop-Location
