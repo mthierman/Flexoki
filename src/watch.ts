60d39efbc2e480d7d3e71a4d0b001111b8ed3376
mthierman <mthierman@gmail.com>
2024-03-05 12:44:01 -0700

    Improve watcher

--- src/watch.ts
+++ src/watch.ts
@@ -2,22 +2,31 @@ import * as esbuild from "esbuild";
 import { exec } from "node:child_process";
 import { dirname, join, resolve } from "node:path";
 import * as process from "node:process";
+import { stdin, stdout } from "node:process";
 import * as readline from "node:readline";
 
-const dir = {
-    src: resolve(dirname(import.meta.dirname), "src"),
-    build: resolve(dirname(import.meta.dirname), "build"),
+const paths = () => {
+    const root = dirname(import.meta.dirname);
+    const src = resolve(root, "src");
+    const build = resolve(root, "build");
+    const input = join(src, "build.ts");
+    const output = join(build, "build.js");
+
+    return {
+        root,
+        src,
+        build,
+        input,
+        output,
+    };
 };
 
-const files = {
-    input: join(dir.src, "build.ts"),
-    output: join(dir.build, "build.js"),
-};
+const cli = readline.createInterface(stdin, stdout);
 
-export const watch = async ({
-    build = dir.build,
-    input = files.input,
-    output = files.output,
+export default async ({
+    build = paths().build,
+    input = paths().input,
+    output = paths().output,
 }: WatchParams = {}) => {
     const plugin: esbuild.Plugin = {
         name: "exec",
@@ -25,7 +34,11 @@ export const watch = async ({
             build.onEnd(async (result) => {
                 const date = new Date();
                 console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-                if (result.metafile) console.log(await esbuild.analyzeMetafile(result.metafile));
+
+                if (result.metafile) {
+                    console.log(await esbuild.analyzeMetafile(result.metafile));
+                }
+
                 exec(`node ${output}`);
             });
         },
@@ -42,26 +55,10 @@ export const watch = async ({
         metafile: true,
     });
 
-    const cleanup = () => {
-        ctx.dispose();
-        console.log("Shutting down...");
-        process.exit();
-    };
-
     await ctx.watch();
 
-    if (process.platform === "win32") {
-        readline
-            .createInterface({
-                input: process.stdin,
-                output: process.stdout,
-            })
-            .on("SIGINT", () => {
-                cleanup();
-            });
-    } else {
-        process.on("SIGINT", () => {
-            cleanup();
-        });
-    }
+    cli.on("close", () => {
+        ctx.dispose();
+        process.exit();
+    });
 };
