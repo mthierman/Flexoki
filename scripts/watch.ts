c48c7979ada8ec393422dae1d3a72a16383e5fb5
mthierman <mthierman@gmail.com>
2024-07-12 11:41:33 -0600

    Remove main function from watch script

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -4,45 +4,41 @@ import { resolve } from "node:path";
 import { stdin, stdout } from "node:process";
 import { createInterface } from "node:readline/promises";
 
-async function main() {
-    const cli = createInterface(stdin, stdout);
+const cli = createInterface(stdin, stdout);
 
-    cli.on("close", () => {
-        console.log("Shutting down...");
-        process.exit();
-    });
+cli.on("close", () => {
+    console.log("Shutting down...");
+    process.exit();
+});
 
-    exec(`pnpm build`);
+exec(`pnpm build`);
 
-    const watcher = watch(resolve(import.meta.dirname, "..", "modules", "flexoki.ts"), {
-        persistent: true,
-        recursive: true,
-    });
+const watcher = watch(resolve(import.meta.dirname, "..", "modules", "flexoki.ts"), {
+    persistent: true,
+    recursive: true,
+});
 
-    let watching = true;
+let watching = true;
 
-    try {
-        for await (const event of watcher) {
-            if (!watching) {
-                exec(`pnpm build`);
-                console.clear();
-                console.log(
-                    `Rebuilding... ${new Date().toLocaleTimeString("en-US", { hour12: false })}`,
-                );
-            }
-
-            watching = true;
-
-            setTimeout(() => {
-                watching = false;
-            }, 50);
+try {
+    for await (const event of watcher) {
+        if (!watching) {
+            exec(`pnpm build`);
+            console.clear();
+            console.log(
+                `Rebuilding... ${new Date().toLocaleTimeString("en-US", { hour12: false })}`,
+            );
         }
-    } catch (error) {
-        console.log(error);
-        if (error instanceof Error) {
-            console.error(error.message);
-        }
+
+        watching = true;
+
+        setTimeout(() => {
+            watching = false;
+        }, 50);
+    }
+} catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+        console.error(error.message);
     }
 }
-
-main();
