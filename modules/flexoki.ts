5f9f9e52b03552e2822100dcc901013f9df4e511
mthierman <mthierman@gmail.com>
2024-07-14 22:15:37 -0600

    Better Classes

--- modules/flexoki.ts
+++ modules/flexoki.ts
@@ -1,5 +1,6 @@
 import Color from "colorjs.io";
 import type { Mode } from "./types.mjs";
+
 class Hex {
     constructor() {}
 
@@ -12,193 +13,122 @@ class Hex {
     }
 }
 
-class Flexoki extends Hex {
+class Base extends Hex {
     constructor() {
         super();
-        this.black = new Color("#100F0F");
-        this.base_950 = new Color("#1C1B1A");
-        this.base_900 = new Color("#282726");
-        this.base_850 = new Color("#343331");
-        this.base_800 = new Color("#403E3C");
-        this.base_700 = new Color("#575653");
-        this.base_600 = new Color("#6F6E69");
-        this.base_500 = new Color("#878580");
-        this.base_300 = new Color("#B7B5AC");
-        this.base_200 = new Color("#CECDC3");
-        this.base_150 = new Color("#DAD8CE");
-        this.base_100 = new Color("#E6E4D9");
-        this.base_50 = new Color("#F2F0E5");
-        this.paper = new Color("#FFFCF0");
-        this.red_600 = new Color("#AF3029");
-        this.orange_600 = new Color("#BC5215");
-        this.yellow_600 = new Color("#AD8301");
-        this.green_600 = new Color("#66800B");
-        this.cyan_600 = new Color("#24837B");
-        this.blue_600 = new Color("#205EA6");
-        this.purple_600 = new Color("#5E409D");
-        this.magenta_600 = new Color("#A02F6F");
-        this.red_400 = new Color("#D14D41");
-        this.orange_400 = new Color("#DA702C");
-        this.yellow_400 = new Color("#D0A215");
-        this.green_400 = new Color("#879A39");
-        this.cyan_400 = new Color("#3AA99F");
-        this.blue_400 = new Color("#4385BE");
-        this.purple_400 = new Color("#8B7EC8");
-        this.magenta_400 = new Color("#CE5D97");
     }
 
-    black;
-    base_950;
-    base_900;
-    base_850;
-    base_800;
-    base_700;
-    base_600;
-    base_500;
-    base_300;
-    base_200;
-    base_150;
-    base_100;
-    base_50;
-    paper;
-    red_600;
-    orange_600;
-    yellow_600;
-    green_600;
-    cyan_600;
-    blue_600;
-    purple_600;
-    magenta_600;
-    red_400;
-    orange_400;
-    yellow_400;
-    green_400;
-    cyan_400;
-    blue_400;
-    purple_400;
-    magenta_400;
+    black = new Color("#100F0F");
+    base_950 = new Color("#1C1B1A");
+    base_900 = new Color("#282726");
+    base_850 = new Color("#343331");
+    base_800 = new Color("#403E3C");
+    base_700 = new Color("#575653");
+    base_600 = new Color("#6F6E69");
+    base_500 = new Color("#878580");
+    base_300 = new Color("#B7B5AC");
+    base_200 = new Color("#CECDC3");
+    base_150 = new Color("#DAD8CE");
+    base_100 = new Color("#E6E4D9");
+    base_50 = new Color("#F2F0E5");
+    paper = new Color("#FFFCF0");
 }
 
-class LightTheme extends Hex {
-    constructor(flexoki: Flexoki) {
+class Accent extends Hex {
+    constructor() {
         super();
-        this.bg = flexoki.paper;
-        this.bg2 = flexoki.base_50;
-        this.ui = flexoki.base_100;
-        this.ui2 = flexoki.base_150;
-        this.ui3 = flexoki.base_200;
-        this.tx3 = flexoki.base_300;
-        this.tx2 = flexoki.base_600;
-        this.tx = flexoki.black;
-        this.re = flexoki.red_600;
-        this.or = flexoki.orange_600;
-        this.ye = flexoki.yellow_600;
-        this.gr = flexoki.green_600;
-        this.cy = flexoki.cyan_600;
-        this.bl = flexoki.blue_600;
-        this.pu = flexoki.purple_600;
-        this.ma = flexoki.magenta_600;
-        this.re2 = flexoki.red_400;
-        this.or2 = flexoki.orange_400;
-        this.ye2 = flexoki.yellow_400;
-        this.gr2 = flexoki.green_400;
-        this.cy2 = flexoki.cyan_400;
-        this.bl2 = flexoki.blue_400;
-        this.pu2 = flexoki.purple_400;
-        this.ma2 = flexoki.magenta_400;
-        this.transparent = new Color("sRGB", [255, 255, 255], 0);
-        this.shadow = new Color("sRGB", [0, 0, 0], 0.25);
     }
 
-    bg;
-    bg2;
-    ui;
-    ui2;
-    ui3;
-    tx3;
-    tx2;
-    tx;
-    re;
-    or;
-    ye;
-    gr;
-    cy;
-    bl;
-    pu;
-    ma;
-    re2;
-    or2;
-    ye2;
-    gr2;
-    cy2;
-    bl2;
-    pu2;
-    ma2;
-    transparent;
-    shadow;
+    red_600 = new Color("#AF3029");
+    orange_600 = new Color("#BC5215");
+    yellow_600 = new Color("#AD8301");
+    green_600 = new Color("#66800B");
+    cyan_600 = new Color("#24837B");
+    blue_600 = new Color("#205EA6");
+    purple_600 = new Color("#5E409D");
+    magenta_600 = new Color("#A02F6F");
+    red_400 = new Color("#D14D41");
+    orange_400 = new Color("#DA702C");
+    yellow_400 = new Color("#D0A215");
+    green_400 = new Color("#879A39");
+    cyan_400 = new Color("#3AA99F");
+    blue_400 = new Color("#4385BE");
+    purple_400 = new Color("#8B7EC8");
+    magenta_400 = new Color("#CE5D97");
 }
 
-class DarkTheme extends Hex {
-    constructor(flexoki: Flexoki) {
+class Light extends Hex {
+    constructor() {
         super();
-        this.bg = flexoki.black;
-        this.bg2 = flexoki.base_950;
-        this.ui = flexoki.base_900;
-        this.ui2 = flexoki.base_850;
-        this.ui3 = flexoki.base_800;
-        this.tx3 = flexoki.base_700;
-        this.tx2 = flexoki.base_500;
-        this.tx = flexoki.base_200;
-        this.re = flexoki.red_400;
-        this.or = flexoki.orange_400;
-        this.ye = flexoki.yellow_400;
-        this.gr = flexoki.green_400;
-        this.cy = flexoki.cyan_400;
-        this.bl = flexoki.blue_400;
-        this.pu = flexoki.purple_400;
-        this.ma = flexoki.magenta_400;
-        this.re2 = flexoki.red_600;
-        this.or2 = flexoki.orange_600;
-        this.ye2 = flexoki.yellow_600;
-        this.gr2 = flexoki.green_600;
-        this.cy2 = flexoki.cyan_600;
-        this.bl2 = flexoki.blue_600;
-        this.pu2 = flexoki.purple_600;
-        this.ma2 = flexoki.magenta_600;
-        this.transparent = new Color("sRGB", [0, 0, 0], 0);
-        this.shadow = new Color("sRGB", [0, 0, 0], 0.25);
     }
 
-    bg;
-    bg2;
-    ui;
-    ui2;
-    ui3;
-    tx3;
-    tx2;
-    tx;
-    re;
-    or;
-    ye;
-    gr;
-    cy;
-    bl;
-    pu;
-    ma;
-    re2;
-    or2;
-    ye2;
-    gr2;
-    cy2;
-    bl2;
-    pu2;
-    ma2;
-    transparent;
-    shadow;
+    base = new Base();
+    accent = new Accent();
+    bg = this.base.paper;
+    bg2 = this.base.base_50;
+    ui = this.base.base_100;
+    ui2 = this.base.base_150;
+    ui3 = this.base.base_200;
+    tx3 = this.base.base_300;
+    tx2 = this.base.base_600;
+    tx = this.base.black;
+    re = this.accent.red_600;
+    or = this.accent.orange_600;
+    ye = this.accent.yellow_600;
+    gr = this.accent.green_600;
+    cy = this.accent.cyan_600;
+    bl = this.accent.blue_600;
+    pu = this.accent.purple_600;
+    ma = this.accent.magenta_600;
+    re2 = this.accent.red_400;
+    or2 = this.accent.orange_400;
+    ye2 = this.accent.yellow_400;
+    gr2 = this.accent.green_400;
+    cy2 = this.accent.cyan_400;
+    bl2 = this.accent.blue_400;
+    pu2 = this.accent.purple_400;
+    ma2 = this.accent.magenta_400;
+    transparent = new Color("sRGB", [255, 255, 255], 0);
+    shadow = new Color("sRGB", [0, 0, 0], 0.25);
+}
+
+class Dark extends Hex {
+    constructor() {
+        super();
+    }
+
+    base = new Base();
+    accent = new Accent();
+    bg = this.base.black;
+    bg2 = this.base.base_950;
+    ui = this.base.base_900;
+    ui2 = this.base.base_850;
+    ui3 = this.base.base_800;
+    tx3 = this.base.base_700;
+    tx2 = this.base.base_500;
+    tx = this.base.base_200;
+    re = this.accent.red_400;
+    or = this.accent.orange_400;
+    ye = this.accent.yellow_400;
+    gr = this.accent.green_400;
+    cy = this.accent.cyan_400;
+    bl = this.accent.blue_400;
+    pu = this.accent.purple_400;
+    ma = this.accent.magenta_400;
+    re2 = this.accent.red_600;
+    or2 = this.accent.orange_600;
+    ye2 = this.accent.yellow_600;
+    gr2 = this.accent.green_600;
+    cy2 = this.accent.cyan_600;
+    bl2 = this.accent.blue_600;
+    pu2 = this.accent.purple_600;
+    ma2 = this.accent.magenta_600;
+    transparent = new Color("sRGB", [0, 0, 0], 0);
+    shadow = new Color("sRGB", [0, 0, 0], 0.25);
 }
 
 class UI extends Hex {
-    constructor(theme: DarkTheme | LightTheme) {
+    constructor(theme: Dark | Light) {
         super();
         this.main_background = theme.bg;
         this.secondary_background = theme.bg2;
@@ -235,7 +165,7 @@ class UI extends Hex {
 }
 
 class Syntax extends Hex {
-    constructor(theme: DarkTheme | LightTheme) {
+    constructor(theme: Dark | Light) {
         super();
         this.comments = theme.tx3;
         this.punctuation = theme.tx2;
@@ -268,28 +198,24 @@ class Syntax extends Hex {
 }
 
 type Theme = {
-    flexoki: Flexoki;
-    theme: DarkTheme | LightTheme;
+    theme: Dark | Light;
     ui: UI;
     syntax: Syntax;
 };
 
 function makeTheme(mode: Mode): Theme {
-    const flexoki = new Flexoki();
     switch (mode) {
         case "Dark": {
-            const theme = new DarkTheme(flexoki);
+            const theme = new Dark();
             return {
-                flexoki: flexoki,
                 theme: theme,
                 ui: new UI(theme),
                 syntax: new Syntax(theme),
             };
         }
         case "Light": {
-            const theme = new LightTheme(flexoki);
+            const theme = new Light();
             return {
-                flexoki: flexoki,
                 theme: theme,
                 ui: new UI(theme),
                 syntax: new Syntax(theme),
@@ -299,4 +225,4 @@ function makeTheme(mode: Mode): Theme {
 }
 
 const theme = makeTheme("Dark");
-console.log(theme.flexoki.hex());
+console.log(theme.ui.hex());
